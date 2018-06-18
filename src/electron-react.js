663c5c4a13d427fe1af2fda5f9c7f57ce4f85414
Hendryanto Fudiko <hendryanto.fudiko@gmail.com>
2018-06-19 00:20:46 +0700

    add css format related file using prettier

--- src/electron-react.js
+++ src/electron-react.js
@@ -1,24 +1,27 @@
-const net = require('net')
-const port = process.env.PORT ? process.env.PORT - 100 : 3002
+const net = require('net');
+const port = process.env.PORT ? process.env.PORT - 100 : 3002;
 
-process.env.ELECTRON_START_URL = `http://localhost:${port}`
+process.env.ELECTRON_START_URL = `http://localhost:${port}`;
 
-const client = new net.Socket()
+const client = new net.Socket();
 
-let startedElectron = false
+let startedElectron = false;
 const tryConnection = () =>
-  client.connect({ port: port }, () => {
-    client.end()
-    if (!startedElectron) {
-      console.log('starting electron')
-      startedElectron = true
-      const exec = require('child_process').exec
-      exec('npm run electron')
+  client.connect(
+    { port: port },
+    () => {
+      client.end();
+      if (!startedElectron) {
+        console.log('starting electron');
+        startedElectron = true;
+        const exec = require('child_process').exec;
+        exec('npm run electron');
+      }
     }
-  })
+  );
 
-tryConnection()
+tryConnection();
 
 client.on('error', error => {
-  setTimeout(tryConnection, 1000)
-})
+  setTimeout(tryConnection, 1000);
+});
